1a58b4d2724281fcbc35e5e25febade33189eaba
John Chavanne <jchavanne@paloaltonetworks.com>
2022-01-19 18:31:49 -0500

    updated provider and bucket config

--- storage.tf
+++ storage.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my-bucket" {
   name          = "${var.project_id}-bucket"
   force_destroy = true
-
+  location      = "US"
   uniform_bucket_level_access = true
 }
